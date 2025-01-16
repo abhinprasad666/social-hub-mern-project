56f09e737626b924febb791067823d083ee287a4
abhinprasad666 <abhinprasad866@gmail.com>
2025-01-16 20:14:28 +0530

    created models folder and user Schema

---
+++ models/user.model.js
@@ -0,0 +1,63 @@
+const { Timestamp } = require('bson')
+const { timeStamp } = require('console')
+const mongoose=require('mongoose')
+
+
+// creating user datas schema
+
+const UserSchema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        minLength:4
+    },
+    fullname:{
+        type:String,
+        required:true,
+        
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+    },
+    followers:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            default:[]
+        }
+    ],
+    following:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        default:[]
+    }],
+    frofileImg:{
+        type:String,
+        default:""
+    },
+    coverImg:{
+        type:String,
+        default:""
+    },
+    bio:{
+        type:String,
+        default:""
+    },
+    link:{
+        type:Sting,
+        default:""
+    },
+
+},{Timestamps:true})
+
+const userModel=mongoose.model('User',UserSchema)
+
+module.exports={userModel}
